af8accbdb9cf1cfcd81761de611369516f9037ba
xieyuxing <185246042@qq.com>
2018-05-18 17:39:00 +0800

    Initial commit

---
+++ src/main/test/com/lingyun/demos/test/shiro/ShiroTest.java
@@ -0,0 +1,68 @@
+package com.lingyun.demos.test.shiro;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.LockedAccountException;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.session.Session;
+import org.apache.shiro.subject.Subject;
+import org.junit.Test;
+
+public class ShiroTest {
+
+    @Test
+    public void test() {
+
+        // 读取 shiro.ini 文件内容
+        IniSecurityManagerFactory factory = new IniSecurityManagerFactory("classpath:shiro.ini");
+        SecurityManager securityManager = factory.getInstance();
+        SecurityUtils.setSecurityManager(securityManager);
+
+        //TODO 为什么是获得单个Subject而不是一个Subject集合?
+        Subject currentUser = SecurityUtils.getSubject();
+
+        Session session = currentUser.getSession();
+        session.setAttribute("someKey", "aValue");
+        String value = (String) session.getAttribute("someKey");
+        if (value.equals("aValue")) {
+            System.out.println("someKey 的值：" + value);
+        }
+
+        // 登陆
+        UsernamePasswordToken token = new UsernamePasswordToken("zhangsan", "zhangsan");
+        token.setRememberMe(true);
+        try {
+            /*
+            TODO
+            通过源码分析，比对密码是否正确是在SimpleCredentialsMatcher的
+            public boolean doCredentialsMatch(AuthenticationToken, AuthenticationInfo)签名方法中完成
+            */
+            currentUser.login(token);
+        } catch (UnknownAccountException uae) {
+            System.out.println("用户名不存在:" + token.getPrincipal());
+        } catch (IncorrectCredentialsException ice) {
+            System.out.println("账户密码 " + token.getPrincipal()  + " 不正确!");
+        } catch (LockedAccountException lae) {
+            System.out.println("用户名 " + token.getPrincipal() + " 被锁定 !");
+        }
+
+        // 认证成功后
+        if (currentUser.isAuthenticated()) {
+
+            System.out.println("用户 " + currentUser.getPrincipal() + " 登陆成功！");
+
+            //测试角色
+            System.out.println("是否拥有 manager 角色：" + currentUser.hasRole("manager"));
+
+            //测试权限
+            System.out.println("是否拥有 user:create 权限" + currentUser.isPermitted("user:create"));
+
+            //退出
+            currentUser.logout();
+        }
+
+    }
+}
