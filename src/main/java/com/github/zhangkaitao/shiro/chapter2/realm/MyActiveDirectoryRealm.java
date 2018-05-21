f3a3babcd672b54a09c5f6f3f6c0803352339844
xieyuxing <185246042@qq.com>
2018-05-21 16:37:01 +0800

    a

---
+++ src/main/java/com/github/zhangkaitao/shiro/chapter2/realm/MyActiveDirectoryRealm.java
@@ -0,0 +1,6 @@
+package com.github.zhangkaitao.shiro.chapter2.realm;
+
+import org.apache.shiro.realm.activedirectory.ActiveDirectoryRealm;
+//TODO 这应该是一个基于URL的Realm
+public class MyActiveDirectoryRealm extends ActiveDirectoryRealm{
+}
--- src/main/test/com/github/zhangkaitao/shiro/chapter2/LoginLogoutTest.java
+++ src/main/test/com/github/zhangkaitao/shiro/chapter2/LoginLogoutTest.java
@@ -7,6 +7,8 @@ import org.apache.shiro.config.IniSecurityManagerFactory;
 import org.apache.shiro.mgt.SecurityManager;
 import org.apache.shiro.subject.Subject;
 import org.apache.shiro.util.Factory;
+import org.apache.shiro.util.ThreadContext;
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Test;
 import sun.security.krb5.Realm;
@@ -50,23 +52,66 @@ public class LoginLogoutTest {
     }
 
     @Test
-    public void  testCustomMultiRealm(){
+    public void testCustomMultiRealm() {
+        //1、获取SecurityManager工厂，此处使用Ini配置文件初始化SecurityManager
+        Factory<org.apache.shiro.mgt.SecurityManager> factory =
+                new IniSecurityManagerFactory("classpath:shiro-multi-realm.ini");
 
-            Factory<SecurityManager> factory =
-                    new IniSecurityManagerFactory("classpath:shiro-multi-realm.ini");
-            SecurityManager securityManager = factory.getInstance();
-            SecurityUtils.setSecurityManager(securityManager);
-            Subject subject = SecurityUtils.getSubject();
-            UsernamePasswordToken token = new UsernamePasswordToken("zhang", "123");
-            try {
-                subject.login(token);
+        //2、得到SecurityManager实例 并绑定给SecurityUtils
+        org.apache.shiro.mgt.SecurityManager securityManager = factory.getInstance();
+        SecurityUtils.setSecurityManager(securityManager);
 
-            } catch (AuthenticationException e) {
-                e.printStackTrace();
-            }
-            Assert.assertEquals(true, subject.isAuthenticated()); //断言用户已经登录
-            //6、退出
-            subject.logout();
+        //3、得到Subject及创建用户名/密码身份验证Token（即用户身份/凭证）
+        Subject subject = SecurityUtils.getSubject();
+        UsernamePasswordToken token = new UsernamePasswordToken("wang", "123");
+
+        try {
+            //4、登录，即身份验证
+            subject.login(token);
+        } catch (AuthenticationException e) {
+            //5、身份验证失败
+            e.printStackTrace();
         }
+
+        Assert.assertEquals(true, subject.isAuthenticated()); //断言用户已经登录
+
+        //6、退出
+        subject.logout();
+    }
+
+
+    @Test
+    public void testJDBCRealm() {
+        //1、获取SecurityManager工厂，此处使用Ini配置文件初始化SecurityManager
+        Factory<org.apache.shiro.mgt.SecurityManager> factory =
+                new IniSecurityManagerFactory("classpath:shiro-jdbc-realm.ini");
+
+        //2、得到SecurityManager实例 并绑定给SecurityUtils
+        org.apache.shiro.mgt.SecurityManager securityManager = factory.getInstance();
+        SecurityUtils.setSecurityManager(securityManager);
+
+        //3、得到Subject及创建用户名/密码身份验证Token（即用户身份/凭证）
+        Subject subject = SecurityUtils.getSubject();
+        UsernamePasswordToken token = new UsernamePasswordToken("zhang", "123");
+
+        try {
+            //4、登录，即身份验证
+            subject.login(token);
+        } catch (AuthenticationException e) {
+            //5、身份验证失败
+            e.printStackTrace();
+        }
+
+        Assert.assertEquals(true, subject.isAuthenticated()); //断言用户已经登录
+
+        //6、退出
+        subject.logout();
+    }
+
+
+    @After
+    public void tearDown() throws Exception {
+        ThreadContext.unbindSubject();//退出时请解除绑定Subject到线程 否则对下次测试造成影响
+    }
 
 }
