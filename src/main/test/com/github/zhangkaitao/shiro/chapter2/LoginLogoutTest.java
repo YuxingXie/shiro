7ecd7eb9d2740b07d84b1f66276917b4c81c1436
xieyuxing <185246042@qq.com>
2018-05-21 14:16:21 +0800

    a

--- src/main/test/com/github/zhangkaitao/shiro/chapter2/LoginLogoutTest.java
+++ src/main/test/com/github/zhangkaitao/shiro/chapter2/LoginLogoutTest.java
@@ -9,13 +9,15 @@ import org.apache.shiro.subject.Subject;
 import org.apache.shiro.util.Factory;
 import org.junit.Assert;
 import org.junit.Test;
+import sun.security.krb5.Realm;
 
 public class LoginLogoutTest {
     @Test
     public void testHelloworld() {
         //1、获取SecurityManager工厂，此处使用Ini配置文件初始化SecurityManager
         Factory<SecurityManager> factory =
-        new IniSecurityManagerFactory("classpath:shiro.ini");
+//        new IniSecurityManagerFactory("classpath:shiro.ini");
+        new IniSecurityManagerFactory("classpath:shiro-realm.ini");
         //2、得到SecurityManager实例 并绑定给SecurityUtils
         org.apache.shiro.mgt.SecurityManager securityManager = factory.getInstance();
         SecurityUtils.setSecurityManager(securityManager);
@@ -32,6 +34,7 @@ public class LoginLogoutTest {
 
             */
             subject.login(token);
+            //TODO 如何通过API获得realms ?
         } catch (AuthenticationException e) {
             //5、身份验证失败
         }
