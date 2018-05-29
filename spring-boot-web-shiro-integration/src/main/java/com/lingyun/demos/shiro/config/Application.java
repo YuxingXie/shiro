0e1927e5f43de0a209118a0d65783f2b676dd831
xieyuxing <185246042@qq.com>
2018-05-29 12:08:34 +0800

    spring integration

---
+++ spring-boot-web-shiro-integration/src/main/java/com/lingyun/demos/shiro/config/Application.java
@@ -0,0 +1,27 @@
+package com.lingyun.demos.shiro.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication()
+@EnableJpaRepositories(basePackages = {"com.lingyun.demos.**.repository"})
+@EnableAutoConfiguration
+@EntityScan(basePackages ={"com.lingyun.demos.**.entity"})
+@ServletComponentScan("com.lingyun.demos.**.filter")
+public class Application implements ApplicationListener<ContextRefreshedEvent> {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+
+    }
+}
