8ae6985bd3e9bcffb63602287233f977bf1e8713
xieyuxing <185246042@qq.com>
2018-06-04 10:39:07 +0800

    spring integration

---
+++ src/test/main/com/github/zhangkaitao/shiro/chapter2/MyAuthorizingRealm.java
@@ -0,0 +1,20 @@
+package com.github.zhangkaitao.shiro.chapter2;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+
+public class MyAuthorizingRealm extends AuthorizingRealm{
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+        return null;
+    }
+
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+        return null;
+    }
+}
